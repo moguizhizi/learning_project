e503be6ab677d0266c38308708ee17266679b7da
moguizhizi <moguizhiz@126.com>
2025-07-04 13:33:17 +0000

    implement basellm initparams

--- example_cpp/basellm.cpp
+++ example_cpp/basellm.cpp
@@ -8,3 +8,35 @@ basellm::basellm(){
 basellm::~basellm(){
     
 }
+
+void basellm::InitParams(){
+    if(this->weight.dicts.find("model_type")!=this->weight.dicts.end()){
+        this->model_type = this->weight.dicts["model_type"].c_str();
+    }
+
+    if(this->weight.dicts.find("bos_token_id")!=this->weight.dicts.end()){
+        this->bos_token_id = atoi(this->weight.dicts["bos_token_id"].c_str());
+    }
+
+    if(this->weight.dicts.find("eos_token_id")!=this->weight.dicts.end()){
+        this->eos_token_id = atoi(this->weight.dicts["eos_token_id"].c_str());
+    }
+
+    if(this->weight.dicts.find("hidden_size")!=this->weight.dicts.end()){
+        this->embed_dim = atoi(this->weight.dicts["hidden_size"].c_str());
+    }
+
+    if(this->weight.dicts.find("num_hidden_layers")!=this->weight.dicts.end()){
+        this->block_cnt = atoi(this->weight.dicts["num_hidden_layers"].c_str());
+    }
+
+    if(this->weight.dicts.find("head_dim")!=this->weight.dicts.end()){
+        this->head_dim = atoi(this->weight.dicts["head_dim"].c_str());
+        this->rotary_dim = this->head_dim;
+    }
+
+    if(this->weight.dicts.find("num_attention_heads")!=this->weight.dicts.end()){
+        this->num_attention_heads = atoi(this->weight.dicts["num_attention_heads"].c_str());
+    }
+
+}
--- example_cpp/qwen3.h
+++ example_cpp/qwen3.h
@@ -4,5 +4,12 @@
 
 class Qwen3Model : public basellm {
   public:
+    std::string model_struct;
+
+    std::string pre_prompt;
+    std::string user_role;
+    std::string bot_role;
+    std::string history_sep;
+
     Qwen3Model();
 };
