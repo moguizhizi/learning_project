cbb3884af0449b08125e791c2f4605934228c466
moguizhizi <moguizhiz@126.com>
2025-07-15 01:58:11 +0000

    ParseDataType

--- example_cpp/fastllm_reproduce/fastllm.cpp
+++ example_cpp/fastllm_reproduce/fastllm.cpp
@@ -1,5 +1,7 @@
 #include "fastllm.h"
 #include "qwen3.h"
+#include <algorithm>
+#include <regex>
 
 basellm *CreateModelWithType(const std::string &model_type) {
     basellm *model = nullptr;
@@ -21,3 +23,34 @@ void AddDictRecursion(basellm *model, const std::string &prefix, const json11::J
 }
 
 bool StringEndWith(const std::string &s, const std::string &end) { return s.size() >= end.size() && s.substr(s.size() - end.size()) == end; }
+bool StringStartWith(const std::string &s, const std::string &end) { return s.size() >= end.size() && s.substr(0, end.size()) == end; }
+void ParseDataType(std::string weightName, const std::vector<std::pair<std::string, std::string>> &dtypeRules, DataType &dataType, int &groupCnt) {
+    std::string matchedType = "";
+    for (int i = 0; i < dtypeRules.size(); i++) {
+        std::regex pattern(dtypeRules[i].first);
+        if (std::regex_search(weightName, pattern)) {
+            matchedType = dtypeRules[i].second;
+        }
+    }
+    transform(matchedType.begin(), matchedType.end(), matchedType.begin(), ::tolower);
+    if (matchedType != "") {
+        for (auto &it : dataTypeNames) {
+            for (auto &dataTypeName : it.second) {
+                if (DefaultGroupCnts.find(it.first) != DefaultGroupCnts.end()) {
+                    if (StringStartWith(matchedType, dataTypeName)) {
+                        dataType = it.first;
+                        if (matchedType != dataTypeName) {
+                            groupCnt = std::atoi(matchedType.substr(dataTypeName.size()).c_str());
+                        } else {
+                            groupCnt = DefaultGroupCnts[it.first];
+                        }
+                    }
+                } else {
+                    if (matchedType == dataTypeName) {
+                        dataType = it.first;
+                    }
+                }
+            }
+        }
+    }
+}
--- example_cpp/fastllm_reproduce/fastllm.h
+++ example_cpp/fastllm_reproduce/fastllm.h
@@ -3,6 +3,24 @@
 #include "basellm.h"
 #include <string>
 
+static std::map<DataType, std::vector<std::string>> dataTypeNames = {{DataType::FLOAT32, {"float32", "fp32"}},
+                                                                     {DataType::BFLOAT16, {"bfloat32", "bf32"}},
+                                                                     {DataType::INT16, {"int16"}},
+                                                                     {DataType::INT8, {"int8"}},
+                                                                     {DataType::INT4, {"int4o"}},
+                                                                     {DataType::INT2, {"int2"}},
+                                                                     {DataType::BIT, {"bit"}},
+                                                                     {DataType::FLOAT16, {"float16", "fp16", "half"}},
+                                                                     {DataType::INT4_NOZERO, {"int4"}},
+                                                                     {DataType::INT4_GROUP, {"int4g"}},
+                                                                     {DataType::FP8_E4M3, {"float8", "fp8", "fp8_e4m3"}},
+                                                                     {DataType::INT2_GROUP, {"int2g"}},
+                                                                     {DataType::BASE3_GROUP, {"base3g"}}};
+
+static std::map<DataType, int> DefaultGroupCnts = {{DataType::INT4_GROUP, 128}, {DataType::INT2_GROUP, 128}, {DataType::BASE3_GROUP, 128}};
+
 basellm *CreateModelWithType(const std::string &model_type);
 void AddDictRecursion(basellm *model, const std::string &prefix, const json11::Json &config);
 bool StringEndWith(const std::string &s, const std::string &end);
+bool StringStartWith(const std::string &s, const std::string &end);
+void ParseDataType(std::string weightName, const std::vector<std::pair<std::string, std::string>> &dtypeConfig, DataType &datatype, int &groupCnt);
