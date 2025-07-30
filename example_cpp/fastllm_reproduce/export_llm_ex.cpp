529a24b54a373afebe4df91b8e8c2cab3c937149
moguizhizi <moguizhiz@126.com>
2025-07-30 12:10:28 +0000

    export_llm_ex.cpp part

--- example_cpp/fastllm_reproduce/export_llm_ex.cpp
+++ example_cpp/fastllm_reproduce/export_llm_ex.cpp
@@ -2,6 +2,7 @@
 #include "file_utils.hpp"
 #include "struct_space.hpp"
 #include <cstring>
+#include <thread>
 
 std::vector<std::pair<std::string, std::string>> ParseDtypeRulesFromConfigString(const std::string &dtypeConfigString) {
     std::vector<std::pair<std::string, std::string>> dtypeRules;
@@ -51,7 +52,7 @@ int main() {
         }
     }
 
-    SafeTensors safetensors(stFiles);
+    SafeTensors safeTensors(stFiles);
 
     // 2. 创建网络基本信息
     std::string configFile = path + "config.json";
@@ -70,7 +71,7 @@ int main() {
 
     model->InitParams();
 
-    std::vector<std::string> tensors = safetensors.GetSortedItemNames();
+    std::vector<std::string> tensors = safeTensors.GetSortedItemNames();
     std::map<std::string, std::vector<std::pair<std::string, DataType>>> tensorMap = model->GetTensorMap(tensors);
     bool useMoeDataType = true;
     DataType moeDataType = DataType::FLOAT16;
@@ -89,10 +90,10 @@ int main() {
 
     DataType lineardtype = DataType::BFLOAT16;
 
-    for (auto &file : safetensors.fileNames) {
+    for (auto &file : safeTensors.fileNames) {
         std::map<std::string, Data> weights;
         std::vector<SafeTensorItem *> item;
-        for (auto &it : safetensors.itmeDict) {
+        for (auto &it : safeTensors.itmeDict) {
             if (it.second.fileName == file) {
                 item.push_back(&it.second);
             }
@@ -129,6 +130,80 @@ int main() {
             } else {
                 weights[weightName] = Data(dataType, it->intShape);
             }
+        }
+
+        std::vector<std::thread *> threads;
+        int thread_num = 16;
+        int per = item.size() / thread_num;
+        int moeGroupCnt = 16;
+        int groupCnt = 128;
+
+        for (int i = 0; i < thread_num; i++) {
+            int st = per * i;
+            int end = (i == thread_num - 1) ? item.size() : per * (i + 1);
+            threads.push_back(new std::thread(
+                [&](int st, int end) {
+                    for (int j = st; j < end; j++) {
+                        auto &tensor = *item[j];
+                        if (StringEndWith(tensor.tensorName, "_scale_inv")) {
+                            continue;
+                        }
+
+                        std::string weightName = tensor.tensorName;
+                        std::string scaleTensorName = "";
+                        DataType oriDataType = DataType::FLOAT32;
+                        DataType dataType = tensorMap[weightName][0].second;
+
+                        int curGroupCnt = model->moelinears.find(weightName) != model->moelinears.end() ? moeGroupCnt : groupCnt;
+                        if ((dataType == DataType::DATA_AUTO_LINEAR || dataType == DataType::DATA_AUTO_CONV) && dtypeRules.size() > 0) {
+                            int groupCnt = -1;
+                            ParseDataType(weightName, dtypeRules, dataType, groupCnt);
+                        }
+
+                        if (dataType >= DataType::DATA_AUTO_NONE) {
+                            dataType = (dataType == DataType::DATA_AUTO_LINEAR || dataType == DataType::DATA_AUTO_CONV) ? lineardtype : oriDataType;
+                        }
+
+                        if (tensor.dtype == "BF16" && (dataType == DataType::FLOAT16 || dataType == DataType::INT8 ||
+                                                       dataType == DataType::INT4_GROUP || dataType == DataType::INT4_NOZERO)) {
+                            oriDataType = DataType::BFLOAT16;
+                        }
+
+                        if (tensor.dtype == "F16" && dataType == DataType::FLOAT16) {
+                            oriDataType = DataType::FLOAT16;
+                        }
+
+                        if (tensor.dtype == "F8_E4M3" &&
+                            (dataType == DataType::FLOAT32 || dataType == DataType::FLOAT16 || dataType == DataType::INT8 ||
+                             dataType == DataType::INT4_GROUP || dataType == DataType::INT4_NOZERO)) {
+                            oriDataType = DataType::FLOAT32;
+                            scaleTensorName = weightName + "_scale_inv";
+                            if (safeTensors.itmeDict.find(scaleTensorName) == safeTensors.itmeDict.end()) {
+                                scaleTensorName = "";
+                            }
+                        }
+
+                        if (tensor.dtype == "F8_E4M3" && (dataType == FP8_E4M3)) {
+                            oriDataType = DataType::FP8_E4M3;
+                            scaleTensorName = tensor.tensorName + "_scale_inv";
+                            if (safeTensors.itmeDict.find(scaleTensorName) == safeTensors.itmeDict.end()) {
+                                scaleTensorName = "";
+                            }
+                        }
+
+                        if (scaleTensorName == "") {
+                            tensor.CreateBuffer(oriDataType);
+                        } else {
+                            auto &scaleTensor = safeTensors.itmeDict[scaleTensorName];
+                            AssertInFastLLM(scaleTensor.dtype == "F32" || scaleTensor.dtype == "BF16",
+                                            "Tensor scale error: scale's dtype should be F32 or BF16.");
+                            scaleTensor.CreateBuffer(DataType::FLOAT32);
+                            tensor.CreateBufferWithScale(dataType, scaleTensor);
+                        }
+                    }
+                },
+                st,
+                end));
         }
     }
 }
