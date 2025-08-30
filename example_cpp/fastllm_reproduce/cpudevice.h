a243bc7ec75b432c05e5df733d70214547369c8e
moguizhizi <moguizhiz@126.com>
2025-08-30 13:50:19 +0000

    FP16SiluManager FP16SigmoidManager

--- example_cpp/fastllm_reproduce/cpudevice.h
+++ example_cpp/fastllm_reproduce/cpudevice.h
@@ -354,3 +354,11 @@ class CpuMatMulTransBOp : BaseOperator {
     void Reshape(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
     void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
 };
+
+class CpuNormalizeOp : BaseOperator {
+    void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+};
+
+class CpuSoftMaxOp : BaseOperator {
+    void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+};
