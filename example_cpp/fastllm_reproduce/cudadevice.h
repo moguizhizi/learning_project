deefd0df0b7c1ca19d20ced5d438a7ac33331ae1
moguizhizi <moguizhiz@126.com>
2025-09-09 00:33:13 +0000

    CudaDevice

--- example_cpp/fastllm_reproduce/cudadevice.h
+++ example_cpp/fastllm_reproduce/cudadevice.h
@@ -1,6 +1,18 @@
 #pragma once // 或者用 #ifndef 方式
 
+#include "basellm.h"
 #include "device.h"
+
+class CudaDevice : BaseDevice {
+  public:
+    CudaDevice();
+
+    bool Malloc(void **ret, size_t size); // 分配尺寸为size的空间
+    bool Free(void *ret);                 // 释放ret
+
+    bool CopyDataToCPU(void *dst, void *src, size_t size);
+    bool CopyDataFromCPU(void *dst, void *src, size_t size);
+};
 
 void DoCudaLinearReshape(Data &input, Data &weight, Data &output);
 
