71b7b65b2fb2a799e7f5ab665c4cf067d26883ec
moguizhizi <moguizhiz@126.com>
2025-10-15 02:23:25 +0000

    FastllmLlamaRotatePosition2DKernel half

--- example_cpp/fastllm_reproduce/fastllm-cuda.cuh
+++ example_cpp/fastllm_reproduce/fastllm-cuda.cuh
@@ -98,6 +98,18 @@ __global__ void FastllmLlamaRotatePosition2DKernel(float *data,
                                                    int partStride,
                                                    int sinCosStride,
                                                    int rotateDim);
+__global__ void FastllmLlamaRotatePosition2DKernel(half *data,
+                                                   float *positionIds,
+                                                   float *sin,
+                                                   float *cos,
+                                                   int len,
+                                                   int bs,
+                                                   int spatial,
+                                                   int n,
+                                                   int m,
+                                                   int partStride,
+                                                   int sinCosStride,
+                                                   int rotateDim);
 
 CudaInfos *getCudaInfos();
 void *FastllmCudaMalloc(size_t);
