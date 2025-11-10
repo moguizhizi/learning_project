30c94d59b70f51db09ef4bf1cd9ee0904836f1b9
devil-son <moguizhiz@126.com>
2025-11-10 11:05:46 +0800

    MultiCudaLinearOp::Run

--- example_cpp/fastllm_reproduce/multicudadevice.cpp
+++ example_cpp/fastllm_reproduce/multicudadevice.cpp
@@ -1,6 +1,7 @@
 #include "multicudadevice.h"
 
 #include "fastllm-cuda.cuh"
+#include "fastllm-multicuda.cuh"
 
 MultiCudaDevice::MultiCudaDevice(CudaDevice *cudaDevice) {
     this->cudaDevice = cudaDevice;
@@ -68,4 +69,60 @@ bool MultiCudaLinearOp::CanRun(const std::string &opType, const DataDict &datas,
     }
     Data &weight = *(datas.find("weight")->second);
     return weight.dims[0] > 10000 || weight.dims[1] > 10000;
+}
+
+void MultiCudaLinearOp::Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams) {
+    Data &input = *(datas.find("input")->second);
+    Data &output = *(datas.find("output")->second);
+    Data &weight = *(datas.find("weight")->second);
+    Data &bias = *(datas.find("bias")->second);
+
+    int n = input.Count(0) / input.dims.back();
+    int m = input.dims.back();
+    int k = output.dims.back();
+
+    int groupCnt = weight.groupCnt;
+    int blockK = weight.blockK;
+    DataType dataType = weight.dataType;
+
+    std::vector<int> devices;
+    std::map<int, int> ratios;
+
+    output.Allocate();
+
+    int uint = (groupCnt = -1 ? 128 : groupCnt);
+    if (dataType == DataType::FP8_E4M3) {
+        uint = blockK;
+    }
+
+    FastllmGetMulticudaDeviceAndRatio(devices, ratios, false);
+    std::vector<int> points = FastllmMultiCudaGetSplitPoints(devices, ratios, k, uint);
+
+    DivisionScheme divisionScheme;
+    for (int i = 0; i < devices.size(); i++) {
+        divisionScheme[devices[i]].push_back(std::make_pair(points[i], points[i + 1]));
+    }
+
+    SplitMultiCudaWeight(weight, bias, devices, divisionScheme, 0);
+    CopyToMultiDevices(input, devices, false);
+    CopyToMultiDevices(output, devices, false);
+
+    auto *pool = GetAlivePool();
+    std::vector<MultiThreadBaseOp *> ops;
+    for (int i = 0; i < devices.size(); i++) {
+        int deviceId = devices[i];
+        int start = points[i];
+        int len = points[i + 1] - points[i];
+        ops.push_back(
+            new MultiCudaDoLinearOp((uint8_t *)input.cudaData, nullptr, input.multiDeviceDatas[deviceId], weight.multiDeviceDatas[deviceId],
+                bias.multiDeviceDatas[deviceId], output.multiDeviceDatas[deviceId], n, m, k, start, len, (uint8_t *)output.cudaData, deviceId));
+    }
+
+    for (int i = 0; i < devices.size(); i++) {
+        pool->PushOp(i, ops[i]);
+    }
+    for (int i = 0; i < devices.size(); i++) {
+        pool->Wait(i);
+        delete ops[i];
+    }
 }
