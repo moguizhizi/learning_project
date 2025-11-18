49e13ec019e74c387b6408759c9b7ec5417ce9a6
devil-son <moguizhiz@126.com>
2025-11-18 22:00:05 +0800

    CudaSoftMaxOp::CanRun

--- example_cpp/fastllm_reproduce/cudadevice.h
+++ example_cpp/fastllm_reproduce/cudadevice.h
@@ -174,3 +174,8 @@ class CudaMatMulOp : BaseOperator {
     void Reshape(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
     void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
 };
+
+class CudaSoftMaxOp : BaseOperator {
+    bool CanRun(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+    void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+};
