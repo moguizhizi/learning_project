b461f413d86b7c451056e9ec56740fcbfa1a0de0
devil-son <moguizhiz@126.com>
2025-11-19 10:04:54 +0800

    CudaAttentionBatchOp::Reshape

--- example_cpp/fastllm_reproduce/cudadevice.h
+++ example_cpp/fastllm_reproduce/cudadevice.h
@@ -235,3 +235,8 @@ class CudaCatDirectBatchOp : BaseBatchOperator {
 class CudaAppendKVCacheBatchOp : BaseBatchOperator {
     void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
 };
+
+class CudaAttentionBatchOp : BaseBatchOperator {
+    void Reshape(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+    void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+};
