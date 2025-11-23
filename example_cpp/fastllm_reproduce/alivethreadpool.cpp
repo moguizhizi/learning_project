79e09a3b93f52a7fb57e5862af106f850b40dde1
devil-son <moguizhiz@126.com>
2025-11-23 17:21:28 +0800

    RunMultiThreadMemcpyMultiLines

--- example_cpp/fastllm_reproduce/alivethreadpool.cpp
+++ example_cpp/fastllm_reproduce/alivethreadpool.cpp
@@ -1,5 +1,7 @@
 #include "alivethreadpool.h"
 
+#include "struct_space.hpp"
+
 AliveThreadTask::AliveThreadTask() {
     signal = 0;
     op = nullptr;
@@ -10,3 +12,23 @@ void AliveThreadPool::PushOp(int tid, MultiThreadBaseOp *op) {}
 void AliveThreadPool::Wait(int tid) {}
 
 void AliveThreadPool::Shutdown() {}
+
+void RunMultiThreadMemcpyMultiLines(std::vector<MultiThreadMemcpyMultiLinesTask> &tasks, AliveThreadPool *pool) {
+    int threadNum = pool->threads.size();
+    int n = tasks.size();
+    int per = n / pool->threads.size();
+    int cur = 0;
+    std::vector<MultiThreadMemcpyMultiLinesOp *> ops;
+    for (int i = 0; i < threadNum; i++) {
+        int end = (i == threadNum - 1 ? n : cur + per + (cur + per * (threadNum - i) < n));
+        ops.push_back(new MultiThreadMemcpyMultiLinesOp(tasks.data(), cur, end));
+        cur = end;
+    }
+    for (int i = 0; i < threadNum; i++) {
+        pool->PushOp(i, ops[i]);
+    }
+    for (int i = 0; i < threadNum; i++) {
+        pool->Wait(i);
+        delete ops[i];
+    }
+}
