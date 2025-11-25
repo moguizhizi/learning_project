cf7c75d8ba0a9dab0e6c9766912576f4685b4ca3
devil-son <moguizhiz@126.com>
2025-11-25 10:50:59 +0800

    GetSpan

--- example_cpp/fastllm_reproduce/utils.cpp
+++ example_cpp/fastllm_reproduce/utils.cpp
@@ -1,10 +1,15 @@
 #include "utils.h"
+
 #include <cmath>
 #include <cstring>
 
-uint32_t as_uint(const float x) { return *(uint32_t *)&x; }
+uint32_t as_uint(const float x) {
+    return *(uint32_t *)&x;
+}
 
-float as_float(const uint32_t x) { return *(float *)&x; }
+float as_float(const uint32_t x) {
+    return *(float *)&x;
+}
 
 float half_to_float(const uint16_t x) {         // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0,
                                                 // +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
@@ -21,7 +26,8 @@ uint16_t float_to_half(const float x) { // IEEE-754 16-bit floating-point format
     const uint32_t e = (b & 0x7F800000) >> 23;  // exponent
     const uint32_t m = b & 0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
     return (b & 0x80000000) >> 16 | (e > 112) * ((((e - 112) << 10) & 0x7C00) | m >> 13) |
-           ((e < 113) & (e > 101)) * ((((0x007FF000 + m) >> (125 - e)) + 1) >> 1) | (e > 143) * 0x7FFF; // sign : normalized : denormalized : saturate
+           ((e < 113) & (e > 101)) * ((((0x007FF000 + m) >> (125 - e)) + 1) >> 1) |
+           (e > 143) * 0x7FFF; // sign : normalized : denormalized : saturate
 }
 
 void Float16ToFloat32(uint16_t *float16, float *float32, int len) {
@@ -120,6 +126,11 @@ void Float16ToBFloat16(uint16_t *float16, uint16_t *bfloat16, int len) {
         bfloat16[i] = (uint16_t)(val >> 16);
     }
 }
+
+double GetSpan(std::chrono::system_clock::time_point time1, std::chrono::system_clock::time_point time2) {
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(time2 - time1);
+    return double(duration.count()) * std::chrono::nanoseconds::period::num / std::chrono::nanoseconds::period::den;
+};
 
 // ===== Manager 构造函数实现 =====
 FP16ToFP32Manager::FP16ToFP32Manager() {
