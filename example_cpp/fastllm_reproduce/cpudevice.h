95f13d8029bde2f21c183aba99037135e20406af
devil-son <moguizhiz@126.com>
2025-12-13 19:37:58 +0800

    CpuAttentionBatchOp

--- example_cpp/fastllm_reproduce/cpudevice.h
+++ example_cpp/fastllm_reproduce/cpudevice.h
@@ -221,6 +221,11 @@ class CpuMatMulTransBBatchOp : BaseBatchOperator {
     void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
 };
 
+class CpuAttentionBatchOp : BaseBatchOperator {
+    void Reshape(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+    void Run(const std::string &opType, const DataDict &datas, const FloatDict &floatParams, const IntDict &intParams);
+};
+
 float erf(float a);
 
 void DoCpuLinearReshape(Data &input, Data &weight, Data &output);
